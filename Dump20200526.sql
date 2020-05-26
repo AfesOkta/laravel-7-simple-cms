bf60ab96ed569718e6d142b1259252577729c0ef
AfesOkta <afes.okta@gmail.com>
2020-05-27 06:49:52 +0700

    MegoEvent V.1.0.0 - 2020-05-27 - send to server

--- Dump20200526.sql
+++ Dump20200526.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `mogomego` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `mogomego`;
+-- CREATE DATABASE  IF NOT EXISTS `mogomego` /*!40100 DEFAULT CHARACTER SET latin1 */;
+-- USE `mogomego`;
 -- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
 --
 -- Host: localhost    Database: mogomego
@@ -21,10 +21,10 @@ USE `mogomego`;
 -- Table structure for table `articles`
 --
 
-DROP TABLE IF EXISTS `articles`;
+DROP TABLE IF EXISTS `mego_articles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `articles` (
+CREATE TABLE `mego_articles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_id` int(10) unsigned NOT NULL,
   `published_at` date NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE `articles` (
   PRIMARY KEY (`id`),
   KEY `articles_published_at_slug_index` (`published_at`,`slug`),
   KEY `articles_category_id_foreign` (`category_id`),
-  CONSTRAINT `articles_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+  CONSTRAINT `articles_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `mego_categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -45,19 +45,19 @@ CREATE TABLE `articles` (
 -- Dumping data for table `articles`
 --
 
-LOCK TABLES `articles` WRITE;
-/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+LOCK TABLES `mego_articles` WRITE;
+/*!40000 ALTER TABLE `mego_articles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mego_articles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS `mego_categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categories` (
+CREATE TABLE `mego_categories` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -73,19 +73,19 @@ CREATE TABLE `categories` (
 -- Dumping data for table `categories`
 --
 
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+LOCK TABLES `mego_categories` WRITE;
+/*!40000 ALTER TABLE `mego_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mego_categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `failed_jobs`
 --
 
-DROP TABLE IF EXISTS `failed_jobs`;
+DROP TABLE IF EXISTS `mego_failed_jobs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `failed_jobs` (
+CREATE TABLE `mego_failed_jobs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -100,19 +100,19 @@ CREATE TABLE `failed_jobs` (
 -- Dumping data for table `failed_jobs`
 --
 
-LOCK TABLES `failed_jobs` WRITE;
-/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+LOCK TABLES `mego_failed_jobs` WRITE;
+/*!40000 ALTER TABLE `mego_failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mego_failed_jobs` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `migrations`
 --
 
-DROP TABLE IF EXISTS `migrations`;
+DROP TABLE IF EXISTS `mego_migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `migrations` (
+CREATE TABLE `mego_migrations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
@@ -124,20 +124,20 @@ CREATE TABLE `migrations` (
 -- Dumping data for table `migrations`
 --
 
-LOCK TABLES `migrations` WRITE;
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2015_04_06_131951_create_categories_table',1),(3,'2015_04_06_131957_create_articles_table',1),(4,'2015_04_06_132351_create_pages_table',1),(5,'2019_08_19_000000_create_failed_jobs_table',1);
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+LOCK TABLES `mego_migrations` WRITE;
+/*!40000 ALTER TABLE `mego_migrations` DISABLE KEYS */;
+INSERT INTO `mego_migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2015_04_06_131951_create_categories_table',1),(3,'2015_04_06_131957_create_articles_table',1),(4,'2015_04_06_132351_create_pages_table',1),(5,'2019_08_19_000000_create_failed_jobs_table',1);
+/*!40000 ALTER TABLE `mego_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `pages`
 --
 
-DROP TABLE IF EXISTS `pages`;
+DROP TABLE IF EXISTS `mego_pages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `pages` (
+CREATE TABLE `mego_pages` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `parent_id` int(10) unsigned DEFAULT NULL,
@@ -155,19 +155,19 @@ CREATE TABLE `pages` (
 -- Dumping data for table `pages`
 --
 
-LOCK TABLES `pages` WRITE;
-/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
+LOCK TABLES `mego_pages` WRITE;
+/*!40000 ALTER TABLE `mego_pages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mego_pages` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `mego_users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+CREATE TABLE `mego_users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -187,10 +187,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin@admin.com','$2y$10$V/D80WrAraFMZ8xiZUAAqOhdruQCCxRgxwI1LId9dAqAZFaBdkmSy','127.0.0.1','DYXiIui1l0ANgEsTXXbVkivsrd6WdtbfwmOumYIk3conOXGaYM8KA7bwBCla','2020-05-26 07:36:52',NULL,'2020-05-26 06:05:42','2020-05-26 07:36:52');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+LOCK TABLES `mego_users` WRITE;
+/*!40000 ALTER TABLE `mego_users` DISABLE KEYS */;
+INSERT INTO `mego_users` VALUES (1,'admin@admin.com','$2y$10$V/D80WrAraFMZ8xiZUAAqOhdruQCCxRgxwI1LId9dAqAZFaBdkmSy','127.0.0.1','DYXiIui1l0ANgEsTXXbVkivsrd6WdtbfwmOumYIk3conOXGaYM8KA7bwBCla','2020-05-26 07:36:52',NULL,'2020-05-26 06:05:42','2020-05-26 07:36:52');
+INSERT INTO `mego_users` VALUES (2,'superadmin@server.com','$10$0PkSLsJQrM4FP935gfQXjOG5TOqm.AEWS1iT9MY2ZFQsLEuWdwoDW','127.0.0.1','DYXiIui1l0ANgEsTXXbVkivsrd6WdtbfwmOumYIk3conOXGaYM8KA7bwBCla','2020-05-26 07:36:52',NULL,'2020-05-26 06:05:42','2020-05-26 07:36:52');
+/*!40000 ALTER TABLE `mego_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
